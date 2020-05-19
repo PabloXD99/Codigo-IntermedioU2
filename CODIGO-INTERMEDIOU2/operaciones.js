362507b88701b36a67941dfa403f6f96c3e37082
unknown <lauromanuelgarciaarzapalo@gmail.com>
2020-05-18 20:36:16 -0500

    funciones e impresion de los triplos

--- CODIGO-INTERMEDIOU2/operaciones.js
+++ CODIGO-INTERMEDIOU2/operaciones.js
@@ -60,3 +60,119 @@ function EcuAritAvanzado() {
                                                   pila.pop(converinfi.charAt(i));//localiza el caracter, y sacalo
                                            };
                       };
+
+                                          } else {//sino del principal pasa lo siguiente
+                            postfi += converinfi.charAt(i); //se concatena el caracter
+                     };//fin 
+                     if (i == converinfi.length - 1) {//si es menor a length pero igual a el tamaño 
+                         while (retornar != "-1") {// mientras retorno no sea -1
+                            postfi += pila.pop();//cada ciclo de la pila concatenalo a postfi
+                            retornar = pila[pila.length - 1];//y el retorno reducelo  1 a 1
+                          };
+                     };//fin 
+              };//fin for
+              return postfi;//retorna el resultado de la conversion
+       };//fin de la funcion
+       
+       //esta es la función de que se compare la busqueda del operador comparado con infijo
+ 
+       function BuscaOperador(caracter){
+              if (caracter == "(" || caracter == "*" || caracter == "/" || caracter == "+" || caracter == "-") {
+                     return true;
+              };
+              return false;
+       };// fin 
+
+       // prioridad de ordenacion con converinfi
+       function PrioridadOperador(operador){
+          
+                     if (operador == "(") {
+                            return 3;
+                     }else{
+                            if (operador == "*" || operador == "/") {
+                                   return 2;
+                            }else{
+                                   if (operador == "+" || operador == "-") {
+                                          return 1;
+                                   };  
+                            };
+                     };             
+        
+       }
+//_________________________________________________________________________
+       //aqui haremos el envio y la entrada infija, a obtencion de la respuesta en postfijo 
+       //y posteriormente lo enviaremos a que sea concatenado a las contadororales.
+
+       let op_original = document.getElementById('ecu').value;
+       if (op_original==""){ 
+              document.getElementById('para').value= "no se ha introducido ninguna operacion";
+       }else{
+       let op_modificado = "";
+       //con el for quitamos los espación de la ecuación origina, si es que los tiene, de lo contrario, solo duplica la ecuación
+       //tambien se puedo haver realizado con un ecu_original.replace(/\s{1,}/gi,'');
+       for(let j = 0; j < op_original.length; j++){
+              if(op_original.charAt(j) != " "){
+                     op_modificado += op_original.charAt(j);
+              };//fin de if
+       };//fin de for
+       
+       let resulPostfijo = "";//recibimos el resultado de conversion
+       resulPostfijo = Infijo_Postfijo(op_modificado);//enviando a la función la ecuación infija.
+       
+       let pilaTemporal = new Array(); //almacena los contadores
+       let impTriplos = new Array(); //almacenara los pop de la pila, para su impresion
+       /// nos serviran para enviar los caracteres de resulPosfijo a la función contadororales
+       let operando1 = "";
+       let operando2 = "";
+       let signo = "";
+       let n = 0;
+
+      
+              for(let k = 0; k <= resulPostfijo.length; k++){
+                     if (BuscaOperador(resulPostfijo.charAt(k))) {//si es un operador haz lo siguiente, de lo contrario es un numero o letra, vete a else
+                            operando1 = pilaTemporal.pop();
+                    
+                            operando2 = pilaTemporal.pop();
+                     
+                            signo = resulPostfijo.charAt(k);
+                     
+                            impTriplos.push(GeneradorTriplo(n, operando1, signo, operando2));
+                            pilaTemporal.push(`t${n}`);
+                            n++;
+                            
+                            
+                            
+                     }else{//por lo general los primeros dos caracteres son numero o letra en una interprestación postfija.
+                            pilaTemporal.push(resulPostfijo.charAt(k));//introducelo a la pila en numero o letra
+                     };
+              };//fin for     
+      
+       impTriplos.push(` ${pilaTemporal.pop()}`);
+       
+       let imprime = new Array(impTriplos);
+       for(var i = 0; i < imprime.length; i++ ){
+             var x = 'x='+'\n';
+             document.getElementById('para').value= x + imprime[i];
+       };return true;
+}
+//};
+//aqui se termina el controlador
+//_________________________________________________________________________________________________
+function GeneradorTriplo(cont, caract1, operacion, caract2) {
+      
+              if (operacion == "*") {
+              return `t${cont} = ${caract2} ${"*"} ${caract1}` + saltoDeLinea;
+              }
+              if (operacion == "/") {
+              return `t${cont} = ${caract2} ${"/"} ${caract1}` + saltoDeLinea;
+              }
+              if (operacion == "+") {
+              return `t${cont} = ${caract2} ${"+"} ${caract1}` + saltoDeLinea
+              }
+              if (operacion == "-") {
+              return `t${cont} = ${caract2} ${"-"} ${caract1}` + saltoDeLinea
+              }
+     
+       return "";
+   }//fin de generadorTriplo
+}//fin de EcuAritAvanzado()
